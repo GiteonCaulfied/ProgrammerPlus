256da2a9815816b835a0209570397bd236277720
Xuzeng He <u7189309@anu.edu.au>
2021-09-25 11:43:05 +0800

    Added HomepageActivity

--- app/src/main/java/au/edu/anu/cecs/COMP6442GroupAssignment/LoginActivity.java
+++ app/src/main/java/au/edu/anu/cecs/COMP6442GroupAssignment/LoginActivity.java
@@ -54,7 +54,7 @@ public class LoginActivity extends AppCompatActivity {
                             Toast.makeText(getApplicationContext(),
                                     "Login successfully.", Toast.LENGTH_SHORT).show();
                             //updateUI(user);
-                            Intent intent = new Intent(LoginActivity.this, PostActivity.class);
+                            Intent intent = new Intent(LoginActivity.this, HomepageActivity.class);
                             intent.putExtra("account",email.getText().toString());
                             startActivity(intent);
                         } else {
