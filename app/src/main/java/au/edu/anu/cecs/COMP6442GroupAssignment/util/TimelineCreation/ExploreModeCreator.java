063234f29948d04ae60c134ff3336a8becc2398e
qinyu.zhao <u7212335@anu.edu.au>
2021-10-22 18:55:09 +0800

    We can scroll time line in the like and explore modes

--- app/src/main/java/au/edu/anu/cecs/COMP6442GroupAssignment/util/TimelineCreation/ExploreModeCreator.java
+++ app/src/main/java/au/edu/anu/cecs/COMP6442GroupAssignment/util/TimelineCreation/ExploreModeCreator.java
@@ -19,6 +19,7 @@ import java.util.Collections;
 import java.util.Random;
 
 import au.edu.anu.cecs.COMP6442GroupAssignment.util.Adapter.TimelinePostAdapter;
+import au.edu.anu.cecs.COMP6442GroupAssignment.util.DAO.UserPostDAO;
 import au.edu.anu.cecs.COMP6442GroupAssignment.util.FirebaseRef;
 import au.edu.anu.cecs.COMP6442GroupAssignment.util.Post;
 
@@ -46,10 +47,7 @@ public class ExploreModeCreator extends TimelineCreator {
      */
     @Override
     public void getData() {
-        getDislikePosts(10);
-    }
-
-    private void getDislikePosts(int postNum) {
+        int postNum = 10;
         db.collection("user-data").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
@@ -123,6 +121,72 @@ public class ExploreModeCreator extends TimelineCreator {
         posts.remove(posts.size() - 1);
         int scrollPosition = posts.size();
         timelinePostAdapter.notifyItemRemoved(scrollPosition);
-        getDislikePosts(posts.size() + 10);
+
+        int postNum = posts.size() + 10;
+        db.collection("user-data").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    Log.d("Read users", "Current data: " + task);
+                    UserLog me = null;
+                    ArrayList<UserLog> userLogs = new ArrayList<>();
+                    for (DocumentSnapshot document : task.getResult()) {
+                        if (document.getId().equals(currentUser.getUid())) {
+                            me = new UserLog(document.getId(), document.getData());
+                        }
+                        else
+                            userLogs.add(new UserLog(document.getId(), document.getData()));
+                    }
+                    if (me == null)
+                        me = new UserLog(currentUser.getUid());
+
+                    for (UserLog userLog: userLogs) {
+                        userLog.setDistanceToMe(me.calDistance(userLog));
+                    }
+                    userLogs.add(me);
+                    Collections.sort(userLogs);
+                    Collections.reverse(userLogs); // reverse the user list
+
+                    Query query = db.collection("user-posts").orderBy("date").limitToLast(postNum * 10);
+                    query.addSnapshotListener(new EventListener<QuerySnapshot>() {
+                        @Override
+                        public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                            if (error != null) {
+                                Log.w("Read posts", "Listen failed.", error);
+                                return;
+                            }
+
+                            if (value != null) {
+                                Log.d("Read posts", "Current data: " + value);
+                                posts.clear();
+                                for (int i = 0; i < userLogs.size(); i++) {
+                                    for (DocumentSnapshot document : value.getDocuments()) {
+                                        Post post = new Post(document.getData());
+                                        if (userLogs.get(i).getPosts().contains(post.getPid())) {
+                                            double r = random.nextDouble();
+                                            if (r <= (userLogs.size() - 1.0 - i) / (userLogs.size() - 1.0)) {
+                                                posts.add(post);
+                                                break;
+                                            }
+                                        }
+                                    }
+                                    if (posts.size() >= postNum)
+                                        break;
+                                }
+
+                                UserPostDAO userPostDAO = UserPostDAO.getInstance();
+                                userPostDAO.setLoading(false);
+                                timelinePostAdapter.notifyDataSetChanged();
+                            } else {
+                                Log.d("Read posts", "Current data: null");
+                            }
+                        }
+                    });
+
+                } else {
+                    Log.d("Read users", "Current data: null");
+                }
+            }
+        });
     }
 }
--- app/src/main/java/au/edu/anu/cecs/COMP6442GroupAssignment/util/TimelineCreation/LikeModeCreator.java
+++ app/src/main/java/au/edu/anu/cecs/COMP6442GroupAssignment/util/TimelineCreation/LikeModeCreator.java
@@ -19,6 +19,7 @@ import java.util.Collections;
 import java.util.Random;
 
 import au.edu.anu.cecs.COMP6442GroupAssignment.util.Adapter.TimelinePostAdapter;
+import au.edu.anu.cecs.COMP6442GroupAssignment.util.DAO.UserPostDAO;
 import au.edu.anu.cecs.COMP6442GroupAssignment.util.FirebaseRef;
 import au.edu.anu.cecs.COMP6442GroupAssignment.util.Post;
 
@@ -41,10 +42,7 @@ public class LikeModeCreator extends TimelineCreator {
 
     @Override
     public void getData() {
-        getLikePosts(10);
-    }
-
-    private void getLikePosts(int postNum) {
+        int postNum = 10;
         db.collection("user-data").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
@@ -121,6 +119,76 @@ public class LikeModeCreator extends TimelineCreator {
         posts.remove(posts.size() - 1);
         int scrollPosition = posts.size();
         timelinePostAdapter.notifyItemRemoved(scrollPosition);
-        getLikePosts(posts.size() + 10);
+
+        int postNum = posts.size() + 10;
+        db.collection("user-data").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    // Read user data
+                    Log.d("Read users", "Current data: " + task);
+                    UserLog me = null;
+                    ArrayList<UserLog> userLogs = new ArrayList<>();
+                    for (DocumentSnapshot document : task.getResult()) {
+                        if (document.getId().equals(currentUser.getUid())) {
+                            me = new UserLog(document.getId(), document.getData());
+                        }
+                        else
+                            userLogs.add(new UserLog(document.getId(), document.getData()));
+                    }
+                    if (me == null)
+                        me = new UserLog(currentUser.getUid());
+
+                    // Calculate the distance between the current user and each other user
+                    for (UserLog userLog: userLogs) {
+                        userLog.setDistanceToMe(me.calDistance(userLog));
+                    }
+                    Collections.sort(userLogs);
+
+
+                    // Read the liked posts of the closest users
+                    Query query = db.collection("user-posts").orderBy("date").limitToLast(postNum * 10L);
+                    query.addSnapshotListener(new EventListener<QuerySnapshot>() {
+                        @Override
+                        public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                            if (error != null) {
+                                Log.w("Read posts", "Listen failed.", error);
+                                return;
+                            }
+
+                            if (value != null) {
+                                Log.d("Read posts", "Current data: " + value);
+                                posts.clear();
+                                for (DocumentSnapshot document : value.getDocuments()) {
+                                    Post post = new Post(document.getData());
+                                    for (int i = 0; i < userLogs.size(); i++) {
+                                        if (userLogs.get(i).getPosts().contains(post.getPid())) {
+                                            double r = random.nextDouble();
+                                            if (r <= (userLogs.size() - 1.0 - i) / (userLogs.size() - 1.0)) {
+                                                posts.add(post);
+                                                break;
+                                            }
+                                        }
+                                    }
+                                    if (posts.size() == postNum)
+                                        break;
+                                }
+
+                                Collections.reverse(posts);
+
+                                UserPostDAO userPostDAO = UserPostDAO.getInstance();
+                                userPostDAO.setLoading(false);
+                                timelinePostAdapter.notifyDataSetChanged();
+                            } else {
+                                Log.d("Read posts", "Current data: null");
+                            }
+                        }
+                    });
+
+                } else {
+                    Log.d("Read users", "Current data: null");
+                }
+            }
+        });
     }
 }
